5484b2eef448ea5d4250ce78c0df1904f664ea34
Goohyun Jung <futuremaker019@gmail.com>
2023-07-10 16:44:39 +0900

    board project seperated

--- 1.building-personal-framework/oop-practice/src/main/java/org/example/calculator/NewArithmeticOperator.java
+++
@@ -1,9 +0,0 @@
-package org.example.calculator.handler;
-
-import org.example.calculator.PositiveNumber;
-
-public interface NewArithmeticOperator {
-
-    boolean support(String operator);
-    int calculate(PositiveNumber operand1, PositiveNumber operand2);
-}
--- 2.board-service/board-practice/README.md
+++
@@ -1,9 +0,0 @@
-## 데모 페이지
-
-https://project-board-noah-387fa977712d.herokuapp.com/
-
-### heroku 사용
-
-```
-
-```
